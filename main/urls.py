ff39c979a7f645ee8300ce1a71522fb3e661c040
parkson-tano <tanoparksonsilencer@gmail.com>
2022-11-06 22:03:18 +0100

    new ui

--- main/urls.py
+++ main/urls.py
@@ -12,6 +12,15 @@ urlpatterns = [
          AdminPostDeleteView.as_view(), name='delete_post'),
     path('adminedit/<int:pk>/', AdminPostUpdateView.as_view(), name='edit_post'),
     path('search/', SearchView.as_view(), name = 'search'),
-    path('admins/post', AdminPostListView.as_view(), name='list_post'),
+    path('admins/post/', AdminPostListView.as_view(), name='list_post'),
+    path('post/<str:slug>/', PostView.as_view(), name='post'),
+    path('category/<str:slug>/', CategoryDetailView.as_view(), name='category'),
+
+    path('admins/create', AdminPostCreateView.as_view(), name='create_post'),
+    path('admins/<int:pk>/delete',
+         AdminPostDeleteView.as_view(), name='delete_post'),
+    path('adminedit/<int:pk>', AdminPostUpdateView.as_view(), name='edit_post'),
+    path('search', SearchView.as_view(), name='search'),
+    path('admins/post/', AdminPostListView.as_view(), name='list_post'),
 
 ]
