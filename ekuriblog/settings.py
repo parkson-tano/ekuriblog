b0f256c0100d5daade46b7343e5fb94f55976906
parkson-tano <tanoparksonsilencer@gmail.com>
2022-11-09 17:59:35 +0100

    fixed done

--- ekuriblog/settings.py
+++ ekuriblog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'ckeditor',
+    'django_quill',
     'crispy_forms',
 
     
--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Comment, Post, Contact
-
+from django_quill.forms import QuillFormField
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
